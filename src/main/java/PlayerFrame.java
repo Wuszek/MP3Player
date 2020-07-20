ba6574be88131a1fd3846a25233c9a2a3acb644c
wiktor.kobiela <wuch.mail@gmail.com>
2020-07-20 17:55:19 +0200

    ref #classes added VolFullMouseListener class

--- src/main/java/PlayerFrame.java
+++ src/main/java/PlayerFrame.java
@@ -153,59 +153,8 @@ public class PlayerFrame extends javax.swing.JFrame{
 
         volUpButton.addMouseListener(new VolUpMouseListener(volUpButton,volfull, mute, volFullButton, muteButton));
 
-        volFullButton.addMouseListener(new MouseAdapter() {
+        volFullButton.addMouseListener(new VolFullMouseListener(volfull, mute, volFullButton, muteButton));
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                super.mouseEntered(e);
-                String image = currentPath+imagePath+"\\volume_full_enabled.png";
-                volFullButton.setIcon(new ImageIcon(image));
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                super.mouseExited(e);
-                if(volfull == false) {
-                  String image2 = currentPath + imagePath + "\\volume_full.png";
-                  volFullButton.setIcon(new ImageIcon(image2));
-                }
-            }
-
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                if(volfull == false) {
-                    volfull = true;
-                    String image = currentPath+imagePath+"\\volume_full_enabled.png";
-                    volFullButton.setIcon(new ImageIcon(image));
-                    try {
-                        new VolumeControl(1);
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    } catch (UnsupportedAudioFileException unsupportedAudioFileException) {
-                        unsupportedAudioFileException.printStackTrace();
-                    }
-                    mute = false;
-                    String image2 = currentPath+imagePath+"\\mute.png";
-                    muteButton.setIcon(new ImageIcon(image2));
-
-                }
-                else if(volfull == true){
-                    volfull = false;
-                    String image = currentPath+imagePath+"\\volume_full.png";
-                    volFullButton.setIcon(new ImageIcon(image));
-                    try {
-                        new VolumeControl(0.5);
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    } catch (UnsupportedAudioFileException unsupportedAudioFileException) {
-                        unsupportedAudioFileException.printStackTrace();
-                    }
-
-                }
-            }
-        });
         muteButton.addMouseListener(new MouseAdapter() {
 
             @Override
