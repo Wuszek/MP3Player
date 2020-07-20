ade360ea11006dd6473b34c47198eb57f1de4e1b
wiktor.kobiela <wuch.mail@gmail.com>
2020-07-20 17:58:09 +0200

    ref #classes added MuteMouseListener class

--- src/main/java/AppTitleMouseListener.java
+++ src/main/java/AppTitleMouseListener.java
@@ -11,35 +11,35 @@ public class AppTitleMouseListener extends MouseAdapter {
     private boolean windowCollapsed;
     private JPanel songNamePanel;
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        super.mouseClicked(e);
-        if(e.getClickCount() == 2){
-            System.out.println("Double clicked");
-            if(windowCollapsed == false){
-                windowCollapsed = true;
-
-                setSize(new Dimension(700, 50));
-
-                //AppTitle.setFont(new Font("Nirmala UI", 0, 12));
-
-                appTitle.setText("Playing now... | " + songFile.getName());
-
-                songNamePanel.setVisible(false);
-                controlPanel.setVisible(false);
-                playlistPanel.setVisible(false);
-
-            } else if (windowCollapsed == true){
-                windowCollapsed = false;
-                setSize(new Dimension(700, 300));
-
-                //AppTitle.setFont(new Font("Nirmala UI", 0, 18));
-                appTitle.setText(title);
-                songNamePanel.setVisible(true);
-                controlPanel.setVisible(true);
-                playlistPanel.setVisible(true);
-
-            }
-        }
-    }
+//    @Override
+//    public void mouseClicked(MouseEvent e) {
+//        super.mouseClicked(e);
+//        if(e.getClickCount() == 2){
+//            System.out.println("Double clicked");
+//            if(windowCollapsed == false){
+//                windowCollapsed = true;
+//
+//                setSize(new Dimension(700, 50));
+//
+//                //AppTitle.setFont(new Font("Nirmala UI", 0, 12));
+//
+//                appTitle.setText("Playing now... | " + songFile.getName());
+//
+//                songNamePanel.setVisible(false);
+//                controlPanel.setVisible(false);
+//                playlistPanel.setVisible(false);
+//
+//            } else if (windowCollapsed == true){
+//                windowCollapsed = false;
+//                setSize(new Dimension(700, 300));
+//
+//                //AppTitle.setFont(new Font("Nirmala UI", 0, 18));
+//                appTitle.setText(title);
+//                songNamePanel.setVisible(true);
+//                controlPanel.setVisible(true);
+//                playlistPanel.setVisible(true);
+//
+//            }
+//        }
+//    }
 }
--- src/main/java/PlayerFrame.java
+++ src/main/java/PlayerFrame.java
@@ -155,57 +155,7 @@ public class PlayerFrame extends javax.swing.JFrame{
 
         volFullButton.addMouseListener(new VolFullMouseListener(volfull, mute, volFullButton, muteButton));
 
-        muteButton.addMouseListener(new MouseAdapter() {
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                super.mouseEntered(e);
-                String image = currentPath+imagePath+"\\mute_enabled.png";
-                muteButton.setIcon(new ImageIcon(image));
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                super.mouseExited(e);
-                if(mute == false) {
-                  String image2 = currentPath + imagePath + "\\mute.png";
-                  muteButton.setIcon(new ImageIcon(image2));
-                }
-            }
-
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                if(mute == false) {
-                  String image = currentPath+imagePath+"\\mute_enabled.png";
-                  muteButton.setIcon(new ImageIcon(image));
-                    try {
-                        new VolumeControl(0);
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    } catch (UnsupportedAudioFileException unsupportedAudioFileException) {
-                        unsupportedAudioFileException.printStackTrace();
-                    }
-                    mute = true;
-                  volfull = false;
-                  String image2 = currentPath+imagePath+"\\volume_full.png";
-                  volFullButton.setIcon(new ImageIcon(image2));
-                }
-                else if(mute == true){
-                    String image = currentPath+imagePath+"\\mute.png";
-                    muteButton.setIcon(new ImageIcon(image));
-                    try {
-                        new VolumeControl(0.5);
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    } catch (UnsupportedAudioFileException unsupportedAudioFileException) {
-                        unsupportedAudioFileException.printStackTrace();
-                    }
-                    mute = false;
-                }
-            }
-        });
+        muteButton.addMouseListener(new MuteMouseListener(volfull, mute, volFullButton, muteButton));
 
         appTitle.addMouseListener(new MouseAdapter() {
             @Override
